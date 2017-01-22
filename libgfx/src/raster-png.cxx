dbde62a541ed9ca3f19a43d6faf682529768a1bf
Sebastian Pipping <sebastian@pipping.org>
2017-01-22 21:52:43 +0100

    libgfx: Fix access to libpng jmpbuf
    Idea from:
    https://github.com/bchretien/choreonoid/commit/9b2155b486c67576fe7775f5da415f9af44df673

--- libgfx/src/raster-png.cxx
+++ libgfx/src/raster-png.cxx
@@ -42,7 +42,7 @@ ByteRaster *read_png_image(const char *file_name)
    // Because we didn't set up any error handlers, we need to be
    // prepared to handle longjmps out of the library on error
    // conditions.
-   if( setjmp(png_ptr->jmpbuf) )
+   if( setjmp(png_jmpbuf(png_ptr)) )
    {
       png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
       fclose(fp);
@@ -132,7 +132,7 @@ bool write_png_image(const char *file_name, const ByteRaster& img)
       return false;
    }
 
-   if( setjmp(png_ptr->jmpbuf) )
+   if( setjmp(png_jmpbuf(png_ptr)) )
    {
       fclose(fp);
       png_destroy_write_struct(&png_ptr,  (png_infopp)NULL);
